469c20608393c2324552738c457b558d7e5168a8
王松 <song.wang@mtime.com>
2018-05-31 20:11:04 +0800

    王松
    try add btn

--- js/fixtab.js
+++ js/fixtab.js
@@ -6,42 +6,35 @@
         //左移事件
         $('.left').on('click', function () {
             var trs = $(that).find("tr");
-            var left_btn_flag = false;
-            var right_btn_flag = false;
 
             //标记
             var view_index = 0;//移动的元素下标标记
             var ths = trs.eq(0).children();
-            for(var i = 0;i<ths.length;i++){
-                var s = tds.eq(i)
+            for(var i = options.column.leftFix;i<ths.length-options.column.rightFix;i++){
+                var s = ths.eq(i)
                 if (s.css("display") != "none") {//找到第一个显示的元素
                     view_index = i;
                     break;
                 }
             }
-            
-
-
-            for (var i in trs) {
-                var tds = trs.eq(i).children();
-                var view_index = 0;//移动的元素下标标记
-                for (var j = options.column.leftFix; j < tds.length - options.column.rightFix; j++) {
-                    var s = tds.eq(j)
-                    if (s.css("display") != "none") {//找到第一个显示的元素
-                        view_index = j;
-                        break;
-                    }
-                }
-                if (view_index + options.column.showNum < tds.length - options.column.rightFix) {//如果还在范围内
+            if (view_index + options.column.showNum < ths.length - options.column.rightFix){//如果还在范围内
+                for(var i=0;i<trs.length;i++){
+                    var tds = trs.eq(i).children();
                     var hide_ele = tds.eq(view_index + options.column.showNum);
                     options.column.hideFunc(tds.eq(view_index));
+                    //隐藏的话就销毁按钮
+                    // if(i==0){
+                    //     createBtn(ths.eq(view_index + options.column.showNum),'right');
+                    //     if(view_index + options.column.showNum+1<ths.length - options.column.rightFix){
+                    //         createTdBtn(ths.eq(view_index+1),'left');
+                    //         //ths.eq(view_index+1).append($(this));
+                    //     }
+                    //     //销毁隐藏的按钮
+                    //     //ths.eq(view_index).find('.btn').remove()
+                       
+                    // }
+                        
                     options.column.showFunc(hide_ele);
-                    left_btn_flag = true;
-                    
-                    // tds.eq(view_index).fadeToggle();
-                    // hide_ele.fadeToggle();
-                    // hide_ele.css('display',tds.eq(view_index).css("display"));
-                    // tds.eq(view_index).css("display",'none');
                 }
             }
         });
@@ -218,25 +211,47 @@ function initTable(options,that){
             upFlag = true;
         }
     }
+    //这里是因为新创建的元素没有绑定click事件。所以先把这些隐藏起来
+    var left_btn = createBtn($(that),'left');
+    left_btn.css("display",'none');
+    var right_btn = createBtn($(that),'right');
+    right_btn.css("display",'none');
+    var up_btn = createBtn($(that),'up');
+    up_btn.css("display",'none');
+    var down_btn = createBtn($(that),'down');
+    down_btn.css("display",'none');
+
     if(upFlag){
         var td_p = trs.eq(options.line.topFix).children().eq(0);
-        createBtn(td_p,'up');
+        createTdBtn(td_p,'up');
 
     }
     if(leftFlag){
         var td_p = trs.eq(0).children().eq(options.column.leftFix);
-        createBtn(td_p,'left');
+        createTdBtn(td_p,'left');
 
     }
+
+
 }
-//创建按钮
+//创建table的隐形按钮
 function createBtn(parent,claz){
     var btn = $('<button></button>').addClass('btn').addClass(claz);
     btn.css('float','left');
     parent.append(btn);
+    return btn;
+}
+//创建td里面的btn
+function createTdBtn(td,claz){
+    console.log(td.text())
+    var btn = td.parent('table').children('.'+claz);
+    
+    td.append(btn);
+    //现在要让其显示出来
+    td.find('.btn').css('display','block');
 }
 
 //销毁按钮
 function destroyBtn(parent){
-    parent.remove('.btn')
+    parent.find('.btn').remove()
 }
